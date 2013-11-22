14b2014c12867f148d9b4b78ba84227b186cd099
Dave Rapin <dave@rapin.com>
2013-11-22 12:02:31 -0500

    added title validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,8 @@ class Page < ActiveRecord::Base
   belongs_to :user
   attr_accessible :title, :page_type, :description, :keywords, :body
 
+  validates :title, :presence => true, length: { in: 2..128 }
+
   def to_param
     "#{id} #{title}".parameterize
   end
